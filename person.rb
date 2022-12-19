b4966205ea3ea959127dbb0811b0ded132dd9789
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-19 20:04:50 -0300

    person file done

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+    sttr_reader :id
+    sttr_accessor :name, :age
+
+    def initialize(id, age, name = "Unknown", parent_permission = true)
+        @id = id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    def is_of_age?
+        @age >= 18
+    end
+
+    def can_use_services?
+        is_of_age? || @parent_permission
+    end
+end
