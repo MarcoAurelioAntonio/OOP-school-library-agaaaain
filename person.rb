01df7d69bc4a79811f1f72972bc233b7076c29a4
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-19 21:54:32 -0300

    Fix offenses

--- person.rb
+++ person.rb
@@ -1,19 +1,19 @@
 class Person
-    sttr_reader :id
-    sttr_accessor :name, :age
+  sttr_reader :id
+  sttr_accessor :name, :age
 
-    def initialize(id, age, name = "Unknown", parent_permission = true)
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def is_of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
-require './person.rb'
+require './person'
 
 class Teacher < Person
-    def initialize(specialization, age, name = "Unknown")
-        super(age, name)
-        @specialization = specialization
-    end
+  def initialize(specialization, age, name = 'Unknown')
+    super(age, name)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
