4f9e35f03c9f9e66a893a898e8f6623edcc8aae4
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-20 21:32:31 -0300

    Decorator file done, and person file test objects

--- person.rb
+++ person.rb
@@ -1,11 +1,12 @@
 require './nameable'
+require './decorator'
 
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -25,3 +26,10 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus      ')
+p person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+p capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p capitalizedTrimmedPerson.correct_name
