86f6d7b81f534c8ca9b5f75b27df1ad69fdffd88
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-22 20:29:28 -0300

    Set relationships between Classroom and student classes

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom # This is the classroom the student belongs to.
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,5 +10,11 @@ class Student < Person
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    # Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
